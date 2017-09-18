81ff78b874d79abb9388ea36f2e321115041f28e
Benz <teepakorn46@gmail.com>
2017-09-18 18:05:43 +0700

    18/09

--- resources/views/Job/index.blade.php
+++ resources/views/Job/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts/app')
 
 @section('content')
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
 
   <h1 align="center" >Job</h1>
   @if(Session::has('message'))
@@ -21,7 +21,7 @@
 
 </div>
 <div align="center" class="col-xs-6"><br></div>
-<table class="table table-bordered" style="width:70%;" align="center">
+<table class="table table-bordered" style="width:80%;" align="center">
     <thead>
         <tr>
             <th>ID</th>
@@ -34,7 +34,7 @@
             <th>telephone</th>
             <th>resume</th>
 
-            <th width="200px">Action</th>
+            <th width="300px">Action</th>
         </tr>
     </thead>
     <tbody>
@@ -49,7 +49,16 @@
               <td> {{ $p['age'] }} </td>
               <td> {{ $p['email'] }} </td>
               <td> {{ $p['telephone'] }} </td>
-              <td> {{ $p['resume'] }} </td>
+              <td>
+
+                <a href="Job/{{$p->resume}}" download="{{$p->resume}}">
+
+                    <i class="">
+                      {{ $p['resume'] }}
+                    </i>
+
+                  </a>
+              </td>
 
 
 
@@ -57,15 +66,10 @@
             {{ Form::open(['route' => ['jobapp.destroy',$p['id'], 'method' => "DELETE"] ]) }}
             <input type="hidden" name="_method" value="delete" />
             {{ Html::link('jobapp/'.$p['id'].'/edit', 'Edit', array('class'=> 'btn btn-primary')) }}
+          
             {{ Form::submit('Delete',array('class' => 'btn btn-primary')) }}
-            <a href="Job/{{$p->resume}}" download="{{$p->resume}}">
-								<button type="button" class="btn btn-primary">
-								<i class="glyphicon glyphicon-download">
-									Download
-								</i>
-								</button>
-							</a>
-  {{ Form::close() }}
+
+            {{ Form::close() }}
 
 
         </td>
